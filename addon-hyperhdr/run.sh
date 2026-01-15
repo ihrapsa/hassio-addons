1d8cf049a49066e192751ca7562c374f048ddfb0
jessica12ryan <jessica12ryan@gmail.com>
2026-01-15 10:10:18 -0400

    Delete addon-hyperhdr/run.sh

--- addon-hyperhdr/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv sh
-
-PORT=$(jq -r '.port // 8090' /data/options.json)
-
-echo "[INFO] Starting HyperHDR on port ${PORT}"
-
-exec /usr/bin/hyperhdr \
-  --userdata /config \
-  --web-port "${PORT}"
